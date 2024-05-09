36ed3e02bf47c5f3a31059ca6e1576517971d187
Sudarshan519 <sudarshanshrestha519@gmail.com>
2024-05-09 09:29:54 +0545

    cors added

--- main.py
+++ main.py
@@ -8,10 +8,20 @@ from pathlib import Path
 import json
 import base64
 
-
-
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "define-your-monment-story.flutterflow.app"
+]
 # Create FastAPI instance
 app = FastAPI()
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
